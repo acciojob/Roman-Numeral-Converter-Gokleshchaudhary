271bff44605bc30354b03391d5d1e39c8c28c844
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-04-29 16:46:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const romanNumerals = [
+    ['M', 1000],
+    ['D', 500],
+    ['C', 100],
+    ['L', 50],
+    ['X', 10],
+    ['V', 5],
+    ['I', 1]
+];
+
 function convertToRoman(num) {
     if (num === 0) return ''; // Roman numerals do not have a representation for 0
     let result = '';
@@ -19,3 +29,17 @@ function convertToRoman(num) {
 
     return result;
 }
+
+document.getElementById('convertButton').addEventListener('click', function() {
+    const numberInput = document.getElementById('numberInput').value;
+    const number = parseInt(numberInput);
+
+    if (isNaN(number)) {
+        document.getElementById('result').innerText = 'Please enter a valid number.';
+    } else if (number < 0 || number > 100000) {
+        document.getElementById('result').innerText = 'Please enter a number between 0 and 100000.';
+    } else {
+        const romanNumeral = convertToRoman(number);
+        document.getElementById('result').innerText = `Roman Numeral: ${romanNumeral}`;
+    }
+});
