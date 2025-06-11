4dc273b7ad27c2f7d00c62ff3fe258804ac6a9dc
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-06-11 09:13:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const romanNumerals = [
+const romanSymbols = [
     ['M', 1000],
     ['D', 500],
     ['C', 100],
@@ -7,40 +7,17 @@ const romanNumerals = [
     ['V', 5],
     ['I', 1]
 ];
-
 function convertToRoman(num) {
-    if (num === 0) return ''; // Roman numerals do not have a representation for 0
     let result = '';
-
-    // Handle special cases first
-    while (num >= 1000) { result += 'M'; num -= 1000; }
-    if (num >= 900) { result += 'CM'; num -= 900; }
-    if (num >= 500) { result += 'D'; num -= 500; }
-    if (num >= 400) { result += 'CD'; num -= 400; }
-    while (num >= 100) { result += 'C'; num -= 100; }
-    if (num >= 90) { result += 'XC'; num -= 90; }
-    if (num >= 50) { result += 'L'; num -= 50; }
-    if (num >= 40) { result += 'XL'; num -= 40; }
-    while (num >= 10) { result += 'X'; num -= 10; }
-    if (num >= 9) { result += 'IX'; num -= 9; }
-    if (num >= 5) { result += 'V'; num -= 5; }
-    if (num >= 4) { result += 'IV'; num -= 4; }
-    while (num >= 1) { result += 'I'; num -= 1; }
-
+    
+    for (let i = 0; i < romanSymbols.length; i++) {
+        while (num >= romanSymbols[i][1]) {
+            result += romanSymbols[i][0];
+            num -= romanSymbols[i][1];
+        }
+    }
+    
     return result;
 }
-
-document.getElementById('convertButton').addEventListener('click', function() {
-    const numberInput = document.getElementById('numberInput').value;
-    const number = parseInt(numberInput);
-
-    if (isNaN(number)) {
-        document.getElementById('result').innerText = 'Please enter a valid number.';
-    } else if (number < 0 || number > 100000) {
-        document.getElementById('result').innerText = 'Please enter a number between 0 and 100000.';
-    } else {
-        const romanNumeral = convertToRoman(number);
-        // Replace 'I' with vertical 'I'
-        document.getElementById('result').innerHTML = `Roman Numeral: ${romanNumeral.replace(/I/g, '<span class="vertical-i">I</span>')}`;
-    }
-});
+console.log(convertToRoman(14));  // Output: XIV
+console.log(convertToRoman(798)); // Output: DCCXCVIII
