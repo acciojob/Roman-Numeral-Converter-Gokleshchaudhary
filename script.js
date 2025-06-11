bf2736fac42ece40cc37fc099c0234c2df563daa
Gokleshchaudhary <123gopalchaudhary123@gmail.com>
2025-06-11 09:45:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,13 +7,30 @@ const romanSymbols = [
     ['V', 5],
     ['I', 1]
 ];
+
 function convertToRoman(num) {
-    // function implementation
+    let result = '';
+
+    for (let i = 0; i < romanSymbols.length; i++) {
+        while (num >= romanSymbols[i][1]) {
+            result += romanSymbols[i][0];
+            num -= romanSymbols[i][1];
+        }
+    }
+    
+    return result;
 }
+
 document.getElementById('convertButton').addEventListener('click', function() {
     const numberInput = document.getElementById('numberInput').value;
     const number = parseInt(numberInput);
-    const romanNumeral = convertToRoman(number); // This should come after the function is defined
+    
+    if (isNaN(number)) {
+        document.getElementById('result').innerText = 'Please enter a valid number.';
+    } else if (number < 0 || number > 100000) {
+        document.getElementById('result').innerText = 'Please enter a number between 0 and 100000.';
+    } else {
+        const romanNumeral = convertToRoman(number);
+        document.getElementById('result').innerText = `Roman Numeral: ${romanNumeral}`;
+    }
 });
-console.log('Function is being called');
-const romanNumeral = convertToRoman(number);
