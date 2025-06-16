f5db3e00d16ba5bda2a9736ce5b1f85cff5694d5
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-06-16 16:32:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
-const romanSymbols = [
-    ['M', 1000],
-    ['D', 500],
-    ['C', 100],
-    ['L', 50],
-    ['X', 10],
-    ['V', 5],
-    ['I', 1]
-];
-
 function convertToRoman(num) {
+    const romanNumerals = [
+        ['M', 1000],
+        ['D', 500],
+        ['C', 100],
+        ['L', 50],
+        ['X', 10],
+        ['V', 5],
+        ['I', 1]
+    ];
+    
     let result = '';
-
-    for (let i = 0; i < romanSymbols.length; i++) {
-        while (num >= romanSymbols[i][1]) {
-            result += romanSymbols[i][0];
-            num -= romanSymbols[i][1];
+    
+    for (let i = 0; i < romanNumerals.length; i++) {
+        const [symbol, value] = romanNumerals[i];
+        
+        // While the number is greater than or equal to the value
+        while (num >= value) {
+            result += symbol; // Append the symbol to the result
+            num -= value; // Subtract the value from num
         }
     }
     
-    return result;
+    return result; // Return the final Roman numeral
 }
 
-document.getElementById('convertButton').addEventListener('click', function() {
-    const numberInput = document.getElementById('numberInput').value;
-    const number = parseInt(numberInput);
-    
-    if (isNaN(number)) {
-        document.getElementById('result').innerText = 'Please enter a valid number.';
-    } else if (number < 0 || number > 100000) {
-        document.getElementById('result').innerText = 'Please enter a number between 0 and 100000.';
-    } else {
-        const romanNumeral = convertToRoman(number);
-        document.getElementById('result').innerText = `Roman Numeral: ${romanNumeral}`;
-    }
-});
+// Example usage:
+console.log(convertToRoman(14));   // Output: XIV
+console.log(convertToRoman(798));
