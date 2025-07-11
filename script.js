8fff1671f7925c5e4f460804a8ecd4a571df35bf
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-07-11 11:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
 function convertToRoman(num) {
-    const romanSymbols = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
+  if (num <= 0 || num > 100000) {
+    return "Please enter a number between 1 and 100000.";
+  }
 
-    let result = '';
+  const romanMap = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1],
+  ];
 
-    // Loop through the symbols
-    for (let i = 0; i < romanSymbols.length; i++) {
-        const [symbol, value] = romanSymbols[i];
-        while (num >= value) {
-            result += symbol; // Append the symbol
-            num -= value;     // Subtract the value from num
-        }
+  let result = '';
+
+  for (let [symbol, value] of romanMap) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
     }
+  }
 
-    return result; // Return the final Roman numeral
+  return result;
 }
 
-document.getElementById('convertButton').addEventListener('click', function() {
-    const num = parseInt(document.getElementById('numberInput').value);
-    if (num < 1 || num > 100000) {
-        document.getElementById('result').innerText = "Please enter a number between 1 and 100000.";
-    } else {
-        const romanNumeral = convertToRoman(num);
-        document.getElementById('result').innerText = romanNumeral;
-    }
-});
+function handleConvert() {
+  const input = document.getElementById("numberInput").value;
+  const number = parseInt(input, 10);
+  const resultElement = document.getElementById("result");
+
+  if (isNaN(number)) {
+    resultElement.textContent = "Please enter a valid number.";
+    return;
+  }
+
+  const roman = convertToRoman(number);
+  resultElement.textContent = `Roman Numeral: ${roman}`;
+}
